60373a0a1b444849e9689ecb523216ab9f87e243
Muhammad Umer <umerqaiser1009@gmail.com>
2023-10-15 18:43:11 +0500

    order status and order get by id

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -1,6 +1,27 @@
 const { Order, CartItem } = require("../models/OrderModel");
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
+exports.findOrderById = (req, res, next, id) => {
+  Order.findById(id)
+    .populate("products.product", "name price")
+    .populate("user", "_id name")
+    .exec()
+    .then((order) => {
+      if (!order) {
+        return res.status(400).json({
+          error: "Order not found",
+        });
+      }
+      req.order = order; //APPEND THE USER IN PROFILE
+      next();
+    })
+    .catch((error) => {
+      return res.status(400).json({
+        error: "Order not found",
+      });
+    });
+};
+
 exports.createOrder = (req, res) => {
   req.body.order.user = req.profile;
   const order = new Order(req.body.order);
@@ -14,4 +35,37 @@ exports.createOrder = (req, res) => {
         error: errorHandler(error),
       });
     });
+};
+
+exports.getAllOrdersForAdmin = (req, res) => {
+  Order.find()
+    .populate("user", "_id name address")
+    .sort("-createdAt")
+    .exec()
+    .then((orders) => {
+      res.json(orders);
+    })
+    .catch((err) => {
+      return res.status(400).json({ error: errorHandler(err) });
+    });
+};
+
+exports.getListOfStatusValuesOfOrders = (req, res) => {
+  res.json(Order.schema.path("status").enumValues);
+};
+
+exports.updateStatusOfOrder = (req, res) => {
+  Order.updateOne({ _id: req.body.orderId }, { $set: { status: req.body.status } })
+    .then((order) => {
+      res.json(order);
+    })
+    .catch((err) => {
+      return res.status(400).json({
+        error: errorHandler(err),
+      });
+    });
+};
+
+exports.getOrder = (req, res) => {
+  return res.json(req.order);
 };
--- routes/OrderRoutes.js
+++ routes/OrderRoutes.js
@@ -1,9 +1,25 @@
 const express = require("express");
 const router = express.Router();
-const { findUserById, addOrderToUserHistory } = require("../controllers/UserController");
-const { requireSignin, isAuth } = require("../controllers/AuthController");
-const { decreaseQunatityOfProductAfterPurchase } = require("../controllers/ProductController");
-const { createOrder } = require("../controllers/OrderController");
+const {
+  findUserById,
+  addOrderToUserHistory,
+} = require("../controllers/UserController");
+const {
+  requireSignin,
+  isAuth,
+  isAdmin,
+} = require("../controllers/AuthController");
+const {
+  decreaseQunatityOfProductAfterPurchase,
+} = require("../controllers/ProductController");
+const {
+  createOrder,
+  getAllOrdersForAdmin,
+  getListOfStatusValuesOfOrders,
+  findOrderById,
+  updateStatusOfOrder,
+  getOrder,
+} = require("../controllers/OrderController");
 
 router.post(
   "/order/create/:userId",
@@ -14,7 +30,40 @@ router.post(
   createOrder //CONTROLLER
 );
 
+router.get(
+  "/orders/list/:userId",
+  requireSignin, //SIGN IN REQUIRED
+  isAuth, //SIGNED IN USER CAN ACCESS
+  isAdmin,
+  getAllOrdersForAdmin //CONTROLLER
+);
+
+router.get(
+  "/order/status-values/:userId",
+  requireSignin, //SIGN IN REQUIRED
+  isAuth, //SIGNED IN USER CAN ACCESS
+  isAdmin,
+  getListOfStatusValuesOfOrders //CONTROLLER
+);
+
+router.get(
+  "/order/:orderId/:userId",
+  requireSignin, //SIGN IN REQUIRED
+  isAuth, //SIGNED IN USER CAN ACCESS
+  isAdmin,
+  getOrder //CONTROLLER
+);
+
+router.put(
+  "/order/:orderId/update-status/:userId",
+  requireSignin, //SIGN IN REQUIRED
+  isAuth, //SIGNED IN USER CAN ACCESS
+  isAdmin,
+  updateStatusOfOrder //CONTROLLER
+);
+
 //param --> wheneever the url or route get the userId variable it goes to that method/controller and get the user by that id
 router.param("userId", findUserById);
+router.param("orderId", findOrderById);
 
 module.exports = router;
